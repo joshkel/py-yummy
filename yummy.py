92cad6c2b052ad7ba8ff4aed8170840c76856fd8
Josh Kelley <joshkel@gmail.com>
2019-01-05 15:38:25 -0500

    Add flake8

--- yummy.py
+++ yummy.py
@@ -6,25 +6,29 @@ from importlib.util import spec_from_loader
 
 # Code based on https://stackoverflow.com/a/43573798/25507
 
+
 class YummyMetaFinder(MetaPathFinder):
     def find_spec(self, fullname, path, target=None):
         if path is None or path == "":
-            path = [os.getcwd()] # top level import --
+            path = [os.getcwd()]  # top level import --
         return spec_from_loader(fullname, loader=YummyLoader(fullname))
+
 
 class YummyLoader(Loader):
     def __init__(self, filename):
         self.filename = filename
 
     def create_module(self, spec):
-        return None # use default module creation semantics
+        return None  # use default module creation semantics
 
     def exec_module(self, module):
         print("yum, %s..." % self.filename)
 
+
 def install():
     """Inserts the finder into the import machinery"""
     sys.meta_path.append(YummyMetaFinder())
+
 
 install()
 
