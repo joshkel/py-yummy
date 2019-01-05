aa359467f4893ea0cf9a0350612ad84a5352d087
Josh Kelley <joshkel@gmail.com>
2019-01-05 16:20:51 -0500

    Initial version

--- yummy.py
+++ yummy.py
@@ -1,16 +1,58 @@
+from io import BytesIO
+import os
+import random
 import sys
-import os.path
 
+import fabulous.image
+import fabulous.utils
 from importlib.abc import Loader, MetaPathFinder
 from importlib.util import spec_from_loader
+import requests
 
-# Code based on https://stackoverflow.com/a/43573798/25507
+from dotenv import load_dotenv
+load_dotenv()
 
+EDAMAM_ENDPOINT = "https://api.edamam.com/search"
+
+# Hack: Fabulous has a Python 2/3 bug that keeps a width parameter from
+# working.  As a workaround, make Python 2's basestring work in Python 3.
+fabulous.image.basestring = str
+
+
+def fetch_recipe(keywords):
+    params = {
+        'q': ' '.join(keywords),
+        'app_id': os.getenv('EDAMAM_APP_ID'),
+        'app_key': os.getenv('EDAMAM_APP_KEY'),
+    }
+    r = requests.get(EDAMAM_ENDPOINT, params=params)
+    results = r.json()
+    return random.choice(results['hits'])['recipe']
+
+
+def print_recipe_text(recipe):
+    print(recipe['label'])
+    print('   (' + recipe['url'] + ')')
+    for ingredient in recipe['ingredientLines']:
+        print(' - ' + ingredient)
+
+
+def print_recipe_image(recipe):
+    image_url = recipe['image']
+    r = requests.get(image_url)
+    print(fabulous.image.Image(BytesIO(r.content), width=fabulous.utils.term.width // 3))
+
+
+# Code for meta finder and loader based on https://stackoverflow.com/a/43573798/25507
 
 class YummyMetaFinder(MetaPathFinder):
+    BLACKLIST = ['nt']
+
     def find_spec(self, fullname, path, target=None):
+        if fullname in self.BLACKLIST:
+            return None
         if path is None or path == "":
-            path = [os.getcwd()]  # top level import --
+            path = [os.getcwd()]  # top level import
         return spec_from_loader(fullname, loader=YummyLoader(fullname))
 
 
@@ -23,6 +65,9 @@ class YummyLoader(Loader):
 
     def exec_module(self, module):
         print("yum, %s..." % self.filename)
+        recipe = fetch_recipe(self.filename.split('_'))
+        print_recipe_text(recipe)
+        print_recipe_image(recipe)
 
 
 def install():
@@ -31,7 +76,3 @@ def install():
 
 
 install()
-
-import fabulous
-import bacon
-
